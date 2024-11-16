560ce03a094b43484e19f86f1c7560f31c92042a
noobcoder123456789 <noobcoder1234567@gmail.com>
2024-11-16 22:05:04 +0700

    add thêm phần fetch GET API printer

---
+++ Web/Frontend/bao/js/get_lich_su_in_an.js
@@ -0,0 +1,123 @@
+function authenticatedFetch(url, options = {}) {
+	const token = localStorage.getItem("token"); // Get token from localStorage
+
+	const headers = {
+		"Content-Type": "application/json",
+		...options.headers,
+	};
+
+	if (token) {
+		headers["Authorization"] = `Bearer ${token}`;
+	}
+	const response = fetch(url, { ...options, headers });
+
+	if (response.status === 401) {
+		console.error(
+			"Không được phép: Token có thể không hợp lệ hoặc đã hết hạn."
+		);
+	}
+
+	return response;
+}
+
+try {
+    const token = localStorage.getItem("token");
+    if (!token) {
+        console.error("No token found. Please log in first.");
+        throw new Error("No token found. Please log in first.");
+    }
+
+    const currentURL = window.location.href;
+    let url = new URL(currentURL);
+    let search_params = url.searchParams;
+
+    authenticatedFetch(
+        `http://localhost:3000/api/printLog/getPrintHistory/admin`, {
+            method: "GET",
+        }
+    ).then((response) => {
+        console.log(response);
+        if (!response.ok) {
+            throw new Error("Failed to fetch printer profile");
+        }
+    
+        const printerInfo = response.json();
+        console.log("printer profile loaded successfully.");
+        console.log("JSON", response);
+
+        printerInfo.then((res) => {
+            let htmls = "";            
+            for(let i = 0; i < res.length; i ++) {
+                let temp = 
+                `<div class="shadow" style="height: 120px; width: 100%; border: 1px solid black; border-radius: 15px; background-color: #FFFFFF; margin-bottom: 20px;">
+                    <div style="width: 40%; height: 120px; background-color: transparent; float: left; display: flex; flex-flow: column wrap; justify-content: space-around; padding: 12px;">
+                        <div style="height: 32px; width: 350px; background-color: transparent;">
+                            <p style="color: black;">
+                                <nobr style="color: #921A40; font-weight: bold; font-size: 14px;">
+                                    Mã đơn in:
+                                </nobr> 
+                                
+                                ${res[i]._id}
+                            </p>
+                        </div>
+
+                        <div style="height: 32px; width: 350px; background-color: transparent;">                            
+                            <p style="color: black;">
+                                <nobr style="color: #921A40; font-weight: bold; font-size: 14px;">
+                                    Mã máy in:
+                                </nobr> 
+                                
+                                ${res[i].printerCode}
+                            </p>
+                        </div>
+
+                        <div style="height: 32px; width: 350px; background-color: transparent;">                            
+                            <p style="color: black;">
+                                <nobr style="color: #921A40; font-weight: bold; font-size: 14px;">
+                                    Tài khoản thực hiện:
+                                </nobr> 
+                                
+                                ${res[i].userName}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div style="width: 30%; height: 121px; background-color: transparent; float: left; display: flex; flex-flow: column wrap; justify-content: flex-start; padding: 12px;">
+                        <div style="height: 32px; width: 350px; background-color: transparent;">                            
+                            <p style="color: black;">
+                                <nobr style="color: #921A40; font-weight: bold; font-size: 14px;">
+                                    Thời gian:
+                                </nobr> 
+                                
+                                ${res[i].createdAt}
+                            </p>
+                        </div>
+
+                        <div style="height: 32px; width: 350px; background-color: transparent;">                            
+                            <p style="color: black;">
+                                <nobr style="color: #921A40; font-weight: bold; font-size: 14px;">
+                                    Ngày in:
+                                </nobr> 
+                                
+                                ${res[i].date}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div style="width: 30%; height: 100%; display: flex; flex-flow: row-reverse wrap; justify-content: flex-start; align-items: flex-end;">
+                        <p type="button" style="font-size: 18px; width: 125px; border-radius: 10px; background-color: #F581A2; margin-right: 15px;" class="text-white btn">
+                            Xem chi tiết
+                        </p>
+                    </div>
+                </div>`;
+
+                htmls += temp;
+            }
+
+            const lich_su_in_an = document.getElementById("lich_su_in_an");
+            lich_su_in_an.innerHTML = htmls;
+        });
+    });    
+} catch (error) {
+    console.error("Error fetching printer profile:", error);
+}
