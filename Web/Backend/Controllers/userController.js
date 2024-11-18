f31f7695bfdcbd9ba2612b7d99e54ee3bc0539c5
Ngô Ngọc Triệu Mẫn <meomeochitchithihi@gmail.com>
2024-11-18 22:33:18 +0700

    thay doi

--- Web/Backend/Controllers/userController.js
+++ Web/Backend/Controllers/userController.js
@@ -65,9 +65,8 @@ exports.createUser = async (req, res) => {
 // Update user information
 exports.updateUser = async (req, res) => {
     try {
-        console.log(req.body);
         const { name, mssv, sdt, email, avatar } = req.body;
-        console.log({name, sdt, email});
+        
         // Find user by ID from the token and update the specified fields
         const updatedUser = await User.findByIdAndUpdate(
             req.user.userId, // lấy userId từ token giải mã trong middleware
